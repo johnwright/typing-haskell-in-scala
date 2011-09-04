59278c8f1279e983f29e54fb7648ddba24878db2
John Wright <john@lshift.net>
2011-09-04 12:27:34 +0100

    Finished off 7.2

--- src/main/scala/typinghaskell/typeclasses.scala
+++ src/main/scala/typinghaskell/typeclasses.scala
@@ -5,7 +5,7 @@ import Subst._
 
 // 7 Type Classes, Predicates and Qualified Types
 
-case class Qual[T <% Types[T]](preds: List[Pred], t: T)
+case class Qual[T <% Types[T]](preds: List[Pred], qualified: T)
 
 sealed abstract class Pred
 
@@ -16,24 +16,24 @@ object Qual {
   import Pred._
   
   implicit def preds2Qual(preds: List[Pred]) = new {
-    def :=>[T <% Types[T]](t: T) = Qual(preds, t)
+    def :=>[T <% Types[T]](qualified: T) = Qual(preds, qualified)
   }
   
-  implicit def qual2Types[T <% Types[T]](qual: Qual[T]): Types[Qual[T]] = new Types[Qual[T]] {
+  implicit def qual2Types[T <% Types[T]](q: Qual[T]): Types[Qual[T]] = new Types[Qual[T]] {
     def apply(subst: Subst) = 
-      qual.preds.apply(subst) :=> qual.t.apply(subst)
+      q.preds.apply(subst) :=> q.qualified.apply(subst)
     
-    def tv: List[TyVar] = qual.preds.tv union qual.t.tv
+    def tv: List[TyVar] = q.preds.tv union q.qualified.tv
   }
 }
 
 object Pred {
   
-  implicit def pred2Type(p: Pred): Types[Pred] = new Types[Pred] {
+  implicit def pred2Types(p: Pred): Types[Pred] = new Types[Pred] {
     def apply(subst: Subst) = p match {
       case IsIn(id, t) => IsIn(id, t.apply(subst))
     }
-  
+    
     def tv: List[TyVar] = p match {
       case IsIn(_, t) => t.tv
     }
@@ -51,21 +51,79 @@ object Pred {
 
 object Class {
   
+  import Qual._
   import Pred._
   
-  type Class = (List[Id], List[Inst])
+  case class Class(supers: List[Id], insts: List[Inst])
   
   type Inst = Qual[Pred]
   
-  case class ClassEnv(classes: Id => Option[Class], defaults: List[Type])
-  
-  def superClasses(ce: ClassEnv, id: Id) = ce.classes(id) match {
-    case Some((ids, _)) => ids
-    case _              => Nil
+  case class ClassEnv(classes: Id => Option[Class], defaults: List[Type]) {
+    
+    def supers(id: Id) = classes(id).map(_.supers).getOrElse(Nil)
+    
+    def insts(id: Id) = classes(id).map(_.insts).getOrElse(Nil)
+    
+    def modify(id: Id, c: Class) =
+      this.copy(classes = (otherId: Id) =>
+        if (id == otherId) Some(c) else classes(otherId))
   }
   
-  def insts(ce: ClassEnv, id: Id) = ce.classes(id) match {
-    case Some((_, instances)) => instances
-    case _                    => Nil
+  val initialEnv = ClassEnv(classes = (id: Id) => None,
+                            defaults = List(TypeHelpers.intType,
+                                            TypeHelpers.doubleType))
+  
+  type EnvTransformer = ClassEnv => Option[ClassEnv]
+  
+  implicit def envTransformer2Composable(f: EnvTransformer) = new {
+    def <:>(g: EnvTransformer) = (env1: ClassEnv) => for {
+      env2 <- f(env1)
+      env3 <- g(env2)
+    } yield env3
   }
+  
+  def addClass(id: Id, supers: Id*): EnvTransformer = (env: ClassEnv) =>
+    if (env.classes(id).isDefined)
+      None // class already defined
+    else if (supers.exists(superId => env.classes(superId).isEmpty))
+      None // superclass not defined
+    else
+      Some(env.modify(id, Class(supers.toList, Nil)))
+  
+  def addPreludeClasses = addCoreClasses <:> addNumClasses
+  
+  def addCoreClasses = (addClass("Eq")
+                    <:> addClass("Ord", "Eq")
+                    <:> addClass("Show")
+                    <:> addClass("Read")
+                    <:> addClass("Bounded")
+                    <:> addClass("Enum")
+                    <:> addClass("Functor")
+                    <:> addClass("Monad"))
+  
+  def addNumClasses = (addClass("Num", "Eq", "Show")
+                   <:> addClass("Real", "Num", "Ord")
+                   <:> addClass("Fractional", "Num")
+                   <:> addClass("Integral", "Real", "Enum")
+                   <:> addClass("RealFrac", "Real", "Fractional")
+                   <:> addClass("Floating", "Fractional")
+                   <:> addClass("RealFloat", "RealFrac", "Floating"))
+  
+  
+  def addInst(inst: Inst): EnvTransformer = (env: ClassEnv) => inst.qualified match {
+    case p @ IsIn(id, _) =>
+      val insts = env.insts(id)
+      val qs = insts.map(_.qualified)
+      val c = Class(env.supers(id), (inst.preds :=> (p: Pred)) :: insts)
+      
+      if (env.classes(id).isEmpty)
+        None // no class for instance
+      else if (qs.exists(overlap(p, _)))
+        None // overlapping instance
+      else
+        Some(env.modify(id, c))
+  }
+  
+  def overlap(p: Pred, q: Pred) =
+    mguPred(p, q).isDefined
 }
